a65821a02e40f2772554e68151aef6eb14434060
raghav-sethi <76265403+raghav-sethi@users.noreply.github.com>
2022-08-14 01:35:16 +0530

    textUtils first commit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,59 @@
 // import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+// import About from "./components/About";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
+  //state that tells that mode is dark or light
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+      showAlert("Light mode on!", "success");
+      document.body.style.backgroundColor = "white";
+    } else {
+      setMode("dark");
+      showAlert("Dark mode on!", "success");
+      document.body.style.backgroundColor = "#042743";
+    }
+  };
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            TextUtils
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  About
-                </a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar
+        title="TextUtils"
+        linkName="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      ></Navbar>
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm
+          showAlert={showAlert}
+          mode={mode}
+          heading="Enter the text to analyze"
+        />
+        {/* <About /> */}
+      </div>
     </>
   );
 }
 
 export default App;
+
+// #212529
---
+++ src/components/TextForm.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("Uppercase");
+    const newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("CONVERTED TO UPPERCASE!", "success");
+  };
+
+  const handleLoClick = () => {
+    // console.log("Uppercase");
+    const newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("converted to lowercase!", "success");
+  };
+
+  const handleClearClick = () => {
+    // console.log("Uppercase");
+    const newText = "";
+    setText(newText);
+    props.showAlert("Cleared!", "success");
+  };
+
+  const handleCopy = () => {
+    let textBox = document.getElementById("myBox");
+    textBox.select();
+    // text.setSelectionRange(0, text.length);
+    navigator.clipboard.writeText(textBox.value);
+    props.showAlert("Copied to Clipboard!", "success");
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ] + /);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success");
+  };
+
+  const handleProperClick = () => {
+    let newWords = [];
+    text.split(" ").forEach((word) => {
+      if (word === "") {
+      } else {
+        word = word.substring(0, 1).toUpperCase() + word.slice(1, word.length);
+        newWords.push(word);
+      }
+    });
+    console.log(newWords);
+    const newText = newWords.join(" ");
+    setText(newText);
+    props.showAlert("Converted To Propercase!", "success");
+  };
+
+  const handleChange = (event) => {
+    // console.log("Change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            onChange={handleChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+            value={text}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleProperClick}>
+          Convert to Propercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+          Remove extra spaces
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear Text
+        </button>
+      </div>
+      <div
+        className="container my-4 "
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters.
+        </p>
+        <p>{text.replace(/ /g, "").length} characters without spaces.</p>
+        <p>{0.008 * text.split(" ").length} Minutes read.</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter something to preview"}</p>
+      </div>
+    </>
+  );
+}
